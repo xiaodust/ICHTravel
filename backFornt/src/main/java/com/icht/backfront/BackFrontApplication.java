5cd01e981deb82808175d0782623b749587864ed
xiao_dust <1599112123@qq.com>
2025-10-09 15:16:21 +0800

    SpringBoot

---
+++ backFornt/src/main/java/com/icht/backfront/BackFrontApplication.java
@@ -0,0 +1,13 @@
+package com.icht.backfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackFrontApplication.class, args);
+	}
+
+}
---
+++ backFornt/src/test/java/com/icht/backfront/BackFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icht.backfront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
